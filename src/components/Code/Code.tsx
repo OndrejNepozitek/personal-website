6eb4df82ad55a6f2b34a8cad241850f414713d9c
OndrejNepozitek <ondra@nepozitek.cz>
2022-01-10 16:25:14 +0100

    Small improvements

--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -9,36 +9,47 @@ import * as styles from "./Code.module.css"
 
 const Code = (props: any) => {
   const className = props.children.props.className || ""
-  const code = props.children.props.children.trim()
   const language = className.replace(/language-/, "")
   const withLinerNumbers = props.children.props.lineNumbers
+  let code = props.children.props.children.trim()
+  let caption = false
+
+  const captionMarker = "$caption:"
+  if (code.startsWith(captionMarker)) {
+    const firstNewline = code.indexOf("\n")
+    caption = code.substring(captionMarker.length, firstNewline)
+    code = code.substring(firstNewline + 1)
+  }
 
   return (
-    <Highlight
-      {...defaultProps}
-      code={code}
-      language={language}
-      // @ts-ignore
-      Prism={Prism}
-      theme={theme}
-    >
-      {({ style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={styles.code} style={style}>
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })} className={styles.line}>
-              {withLinerNumbers && (
-                <span className={styles.lineNumber}>{i + 1}</span>
-              )}
-              <span className={styles.lineContent}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </span>
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+    <div className={styles.codeWrapper}>
+      <Highlight
+        {...defaultProps}
+        code={code}
+        language={language}
+        // @ts-ignore
+        Prism={Prism}
+        theme={theme}
+      >
+        {({ style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={styles.code} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })} className={styles.line}>
+                {withLinerNumbers && (
+                  <span className={styles.lineNumber}>{i + 1}</span>
+                )}
+                <span className={styles.lineContent}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </span>
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+      {caption && <div className={styles.caption}>{caption}</div>}
+    </div>
   )
 }
 
