cbb8921b99f3ee96306e988436ee248d9f1fa398
OndrejNepozitek <ondra@nepozitek.cz>
2022-03-02 16:57:39 +0100

    Updates

--- component---src-pages-404-js-965fd098a6b2236d75ca.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),l=a(5700),s=a.n(l),u=a(3721);e.default=function(t){var e=t.data,a=t.location,l=e.site.siteMetadata.title;return n.createElement(s(),{location:a,title:l},n.createElement(u.default,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-965fd098a6b2236d75ca.js.map
